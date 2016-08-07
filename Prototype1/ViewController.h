b93e8e766d2dc71cd77460ad88a12e37724c3f7f
Saranya Rathinasamy <saranyarathinasamy10@gmail.com>
2016-08-07 16:16:11 +0530

    Initial Commit

---
+++ Prototype1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Prototype1
+//
+//  Created by Saranya Rathinasamy on 07/08/16.
+//  Copyright (c) 2016 Saranya Rathinasamy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Prototype1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Prototype1
+//
+//  Created by Saranya Rathinasamy on 07/08/16.
+//  Copyright (c) 2016 Saranya Rathinasamy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
